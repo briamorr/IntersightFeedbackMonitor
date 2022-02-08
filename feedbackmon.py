9f0ded8584aeff042d59c6d938187a1974e3be01
briamorr <78086763+briamorr@users.noreply.github.com>
2022-02-08 08:45:26 -0500

    Create feedbackmon.py

---
+++ feedbackmon.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from datetime import datetime, timedelta
+
+from intersight_auth import IntersightAuth
+
+def getFeedback():
+    #Determine the last X minutes, days, hours from now for how far back to return in the feedback filter
+    timeDelta = datetime.utcnow() - timedelta(days=7)
+    timeDelta = str(timeDelta.strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+    RESPONSE = requests.request(
+        method="GET",
+        url="https://www.intersight.com/api/v1/aaa/AuditRecords?$top=10&$select=Email,CreateTime,Request&$filter=MoType eq 'feedback.FeedbackPost' and CreateTime gt " + timeDelta + "&$orderby=CreateTime desc",
+        auth=AUTH
+    )
+
+    feedbacks = RESPONSE.json()["Results"]
+    for r in feedbacks:
+        try:
+           print(r['CreateTime'],r['Email'],"\n",r['Request']['FeedbackData']['Comment'])
+        except:
+           print("")
+
+#Configure Intersight API connection, requires minimum of "Audit Log Viewer" privilege        
+AUTH = IntersightAuth(
+    secret_key_filename='SecretKey.txt',
+    api_key_id='xxxxxx/yyyyyy/zzzzzz'
+    )
+
+getFeedback()
